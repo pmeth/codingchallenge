1f838370e4800413133ff4526d52f9e9bf808009
Peter Meth <pmeth@centah.com>
2013-06-13 10:37:57 -0400

    all single digit combos in forward order

---
+++ combinations.php
@@ -0,0 +1,21 @@
+<?php
+
+print_r(combinations(range(1, 3)));
+
+function combinations(array $buttons) {
+	$return = array();
+	foreach ($buttons as $key => $button) {
+		unset($buttons[$key]);
+		$combinations = combinations($buttons);
+		$return[] = $button;
+		foreach($combinations as $combination) {
+			// do combinations that include button
+			$return[] = $button . ' ' . $combination;
+			// do combinations that don't include button
+			$return[] = $combination;
+
+		}
+	}
+
+	return array_unique($return);
+}
